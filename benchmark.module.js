cef001c1a7f0b97dba4158dbe0904979dd87eaef
gunjan kumar <gunjan_kumar@homedepot.com>
2016-05-19 13:46:07 -0400

    initial commit

---
+++ benchmark.module.js
@@ -0,0 +1,33 @@
+"use strict"
+
+const benchrest = require('bench-rest');
+
+
+var flow = {
+    before: [],      // operations to do before anything 
+    beforeMain: [],  // operations to do before each iteration 
+    main: [  // the main flow for each iteration, #{INDEX} is unique iteration counter token 
+      { put: 'http://localhost:8000/foo_#{INDEX}', json: 'mydata_#{INDEX}' },
+      { get: 'http://localhost:8000/foo_#{INDEX}' }
+    ],
+    afterMain: [{ del: 'http://localhost:8000/foo_#{INDEX}' }],   // operations to do after each iteration 
+    after: []        // operations to do after everything is done 
+  };
+ 
+  module.exports = flow;
+ 
+  var runOptions = {
+    limit: 10,         // concurrent connections 
+    iterations: 1000,  // number of iterations to perform 
+    prealloc: 100      // only preallocate up to 100 before starting 
+  };
+  var errors = [];
+  benchrest(flow, runOptions)
+    .on('error', function (err, ctxName) { console.error('Failed in %s with err: ', ctxName, err); })
+    .on('progress', function (stats, percent, concurrent, ips) {
+      console.log('Progress: %s complete', percent);
+    })
+    .on('end', function (stats, errorCount) {
+      console.log('error count: ', errorCount);
+      console.log('stats', stats);
+    });
